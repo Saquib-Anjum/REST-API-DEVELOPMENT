56cdfdd0f648ba5a0aeb7b6b3c71a2758a334235
Saquib Anjum <saquibanjum8969@gmail.com>
2025-02-10 00:48:17 +0530

    ....

--- controllers/auth/refreshController.js
+++ controllers/auth/refreshController.js
@@ -1,6 +1,11 @@
+import Joi from 'joi'
 const refreshController={
 async refresh(req,res,next){
 //validate request 
+const refreshTokenSchema = Joi.object({
+    token: Joi.string().email().unique(),
+    
+  });
 }
 }
 export default refreshController;

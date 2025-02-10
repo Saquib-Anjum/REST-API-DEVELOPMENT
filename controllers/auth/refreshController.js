c8d05d79bf0796d9b9d4367b6c943cec12c1abac
Saquib Anjum <saquibanjum8969@gmail.com>
2025-02-10 22:29:26 +0530

    ___

--- controllers/auth/refreshController.js
+++ controllers/auth/refreshController.js
@@ -3,9 +3,16 @@ const refreshController={
 async refresh(req,res,next){
 //validate request 
 const refreshTokenSchema = Joi.object({
-    token: Joi.string().email().unique(),
+    token: Joi.string().required().unique(),
     
   });
+  const {error} = refreshTokenSchema.validate(req.body);
+
+  if(error){
+    return next(error);
+  }
+  //database checking 
+  
 }
 }
 export default refreshController;
